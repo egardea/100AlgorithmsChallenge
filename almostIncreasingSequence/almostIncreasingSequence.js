34a467754bb6e293204029cdc87b4f2d6719750d
Erik Gardea <erik1gardea@gmail.com>
2019-11-21 00:20:38 -0600

    almostIncreasingSquence

--- almostIncreasingSequence/almostIncreasingSequence.js
+++ almostIncreasingSequence/almostIncreasingSequence.js
@@ -1,13 +1,18 @@
 function almostIncreasingSequence(sequence) {
-    let newSequence = [];
+    let errors = 0;
     for(let i =  0; i < sequence.length; i++) {
-        if(sequence[i] < sequence[i + 1]) {
-            return true
-        } else if() {
-            
+        if(sequence[i] <= sequence[i - 1]) {
+            errors++;
+            if(sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
+                return false
+            }
         }
     }
+    return errors <= 1;
 };
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2]))
+ console.log(almostIncreasingSequence([1, 3, 1, 1])) 
+ console.log(almostIncreasingSequence([1, 3, 2]))
+ console.log(almostIncreasingSequence([3, 2, 1]))
+
+
