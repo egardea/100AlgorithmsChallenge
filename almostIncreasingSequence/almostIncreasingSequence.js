58aa016935637f741fc016a9b383d7777125aea8
Erik Gardea <erik1gardea@gmail.com>
2019-11-20 23:48:32 -0600

    almostIncreasingSequence

---
+++ almostIncreasingSequence/almostIncreasingSequence.js
@@ -0,0 +1,13 @@
+function almostIncreasingSequence(sequence) {
+    let newSequence = [];
+    for(let i =  0; i < sequence.length; i++) {
+        if(sequence[i] < sequence[i + 1]) {
+            return true
+        } else if() {
+            
+        }
+    }
+};
+
+// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+// console.log(almostIncreasingSequence([1, 3, 2]))
