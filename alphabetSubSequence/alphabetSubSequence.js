4e852e3743babd17a3039225fae85ad51ce84cdb
Erik Gardea <erik1gardea@gmail.com>
2019-11-24 22:53:55 -0600

    alphabeticShift

--- alphabetSubSequence/alphabetSubSequence.js
+++ alphabetSubSequence/alphabetSubSequence.js
@@ -1,13 +1,17 @@
 function alphabetSubsequence(s) {
+    //turn the string into an array
     const stringToArr = s.split('');
+    //iterate through and find the char code
     let newArr = [];
     for(let i = 0; i < stringToArr.length; i++){
         newArr.push(stringToArr[i].charCodeAt())
     }
+    //use new Set to remove duplicates and if there are, then return false
     console.log(newArr);
     if(new Set(newArr).size !== newArr.length){
         return false;
     }
+    //compare the and return false if there is no sequence
     console.log(new Set(newArr));
     if(newArr[0] >= newArr[1]) {
         return false;
---
+++ alphabeticShift/alphabeticShift.js
@@ -0,0 +1,26 @@
+function alphabeticShift(inputString) {
+    //turn the string into an array
+    const stringToArr = inputString.split('');
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    //turn the alphabet string into an array
+    const alphabetToArr = alphabet.split('');
+    //iterate through the input string array
+    let indexOfChar = [];
+    for(let i = 0; i < stringToArr.length; i++) {
+        //find the index in the alphabet array and add 1 to it
+        if(stringToArr[i] !== 'z') {
+            indexOfChar.push(alphabetToArr.indexOf(stringToArr[i]) + 1);
+        } else if(stringToArr[i] === 'z') {
+            indexOfChar.push(0);
+        }
+    };
+    
+    //iterate through the indexofchar array to return the right letter
+    let alphabeticShift = [];
+    for(let i = 0; i < indexOfChar.length; i++) {
+        alphabeticShift.push(alphabetToArr[indexOfChar[i]]);
+    };
+    return alphabeticShift.join('');
+};
+
+console.log(alphabeticShift('crazy'));
