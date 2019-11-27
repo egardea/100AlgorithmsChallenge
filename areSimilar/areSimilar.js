40ab42d39d3d6201bf70cf518bbfb0a7139407ff
Erik Gardea <erik1gardea@gmail.com>
2019-11-27 00:58:42 -0600

    areSimilar

---
+++ areSimilar/areSimilar.js
@@ -0,0 +1,20 @@
+function areSimilar(a, b) {
+    if(a.toString() === b.toString()) {
+        return true;
+    }
+    let errors = [];
+    for(let i = 0; i < a.length; i++) {
+        if(a[i] !== b[i]) {
+            errors.push(a[i]);
+            errors.reverse();
+        } else {
+            errors.push(a[i]);
+        }
+    }
+    console.log(errors);
+    return errors.toString() !== b.toString() ? false : true;
+};
+
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
