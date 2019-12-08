6226868b59cb7d84ffe4388c9c893abedcd66e05
Erik Gardea <erik1gardea@gmail.com>
2019-12-08 15:31:39 -0600

    added two more parts to the solution of arrayPreviousLess

--- arrayPreviousLess/arrayPreviousLess.js
+++ arrayPreviousLess/arrayPreviousLess.js
@@ -1,11 +1,17 @@
 function arrayPreviousLess(items) {
+    let newItems = [...items];
     for(let i = 0; i < items.length; i++) {
         if(items[i] <=  items[i - 1]){
-            items[i - 1] = -1;
-            items.unshift(-1);
+            newItems[i - 1] = -1;
         }
-    }
-    return items;
+    };
+
+    newItems.unshift(-1);
+
+    if(items[items.length - 1] >= items[0]) {
+        newItems.pop();
+    };
+    return newItems;
 };
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
