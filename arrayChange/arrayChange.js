8826c6e8b6c8bedc88e6f765efb349b348711795
egardea <erik1gardea@gmail.com>
2019-12-17 20:19:01 -0600

    changed the solution on arrayChange

--- arrayChange/arrayChange.js
+++ arrayChange/arrayChange.js
@@ -1,15 +1,11 @@
 function arrayChange(inputArray) {
     let count = 0;
     
-
     for(let i = 0; i < inputArray.length; i++) {
-        if(inputArray[i] === inputArray[i + 1]) {
-            console.log(inputArray[i]);
-            count++;
-            if(inputArray[i + 1] === inputArray[i + 2]) {
-                console.log(inputArray[i]);
-                count++;
-            }
+        if(inputArray[i] >= inputArray[i + 1]) {
+            let dif = inputArray[i] + 1 - inputArray[i + 1];
+            inputArray[i + 1] = inputArray[i] + 1;
+            count += dif;
         }
     }
 
@@ -17,3 +13,4 @@ function arrayChange(inputArray) {
 }
 
 console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([2, 1, 10, 1])); /**output should be 12 */
