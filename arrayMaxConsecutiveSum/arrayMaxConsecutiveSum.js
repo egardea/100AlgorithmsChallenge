847776b27e19752f40054421957ad9c9c6fb8dfc
egardea <erik1gardea@gmail.com>
2019-12-19 19:12:35 -0600

    added to the solution for arrayMaxConsecutiveSum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
@@ -1,11 +1,18 @@
 function arrayMaxConsecutiveSum(inputArray, k) {
     let sum = [];
 
-    for(let i = 0; i < inputArray.length - 1; i++) {
-        sum.push(inputArray[i] + inputArray[i + 1]);
+    for(let i = 0; i < inputArray.length - `${k - 1}`; i++) {
+        if(k === 2) {
+            sum.push(inputArray[i] + inputArray[i + 1]);
+        } else if(k === 3) {
+            sum.push(inputArray[i] + inputArray[i + 1] + inputArray[i + 2]);
+        }
     }
 
+    sum = Math.max(...sum);
+
     return sum;
+
 };
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
