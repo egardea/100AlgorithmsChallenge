65600d1f14fbd8cd25edb02d588a917aa06b51a8
egardea <erik1gardea@gmail.com>
2020-03-04 19:51:04 -0600

    refactored the solution

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
@@ -1,16 +1,33 @@
 function arrayMaxConsecutiveSum(inputArray, k) {
+    //if the k value is one then we return the highest number in the array
+    if(k === 1) return inputArray = Math.max(...inputArray);
+
+    //declare an empty array to push our sliced arrays to
     let sum = [];
 
-    for(let i = 0; i < inputArray.length - `${k - 1}`; i++) {
-        if(k === 2) {
-            sum.push(inputArray[i] + inputArray[i + 1]);
-        } else if(k === 3) {
-            sum.push(inputArray[i] + inputArray[i + 1] + inputArray[i + 2]);
+    //declare a copy of inputarray to modify during the loop
+    const newInputArr = [...inputArray];
+    for(let i = 0; i < newInputArr.length; i++) {
+        //only slice up to k length and remove the first index in the array if the k value is greater than the length of the array
+        if(newInputArr.length > k) {
+            sum.push(newInputArr.slice(0, k));
+            newInputArr.shift();
         }
     }
+    //push the last values of the array equal to k
+    if(newInputArr.length === k) {
+        sum.push(newInputArr);
+    }
+    
+    //loop through the sum array and add the values inside those arrays
+    for(let i = 0; i < sum.length; i++) {
+        sum[i] = sum[i].reduce((acc, cur) =>  acc + cur)
+    }
 
+    //reassign sum to the largest number
     sum = Math.max(...sum);
 
+    //return sum
     return sum;
 
 };
